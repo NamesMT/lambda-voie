ef4f99dba574cd015d669f738f99819ad061ba94
NamesMT <dangquoctrung123@gmail.com>
2023-12-29 15:02:36 +0700

    fix(response): autoCors broke because response no longer stringified

--- src/index.ts
+++ src/index.ts
@@ -321,7 +321,7 @@ export class Voie extends Router {
       (
         event.requestContext?.http?.method !== 'OPTIONS' // Bypass on OPTIONS call
         && event.route.method // Make sure this a valid URL invoke
-        && this._lookupShims(fakeEvent('OPTIONS', event.route.path)).body === '"cors"' // Finally, tries the OPTIONS route to see if cors is enabled.
+        && this._lookupShims(fakeEvent('OPTIONS', event.route.path)).body === 'cors' // Finally, tries the OPTIONS route to see if cors is enabled.
       ), // #2
       compress,
       contentType = 'application/json',
