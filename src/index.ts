94373bb0561b4b802d6d47eaa77fa3ea69f6e731
NamesMT <dangquoctrung123@gmail.com>
2023-12-29 15:06:42 +0700

    fix: options.contentType

--- src/index.ts
+++ src/index.ts
@@ -353,7 +353,7 @@ export class Voie extends Router {
 
         ...contentType
           ? {
-              'Content-Type': 'application/json',
+              'Content-Type': contentType,
             }
           : undefined,
 
